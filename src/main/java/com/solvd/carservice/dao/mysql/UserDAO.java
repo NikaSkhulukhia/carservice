c466e8625d4767da50ca2e25dd49494e49524c7e
Nika Skhulukhia <nskhu12@gmail.com>
2023-01-20 11:36:11 +0400

    implement get entity by id inside userdao

--- src/main/java/com/solvd/carservice/dao/mysql/UserDAO.java
+++ src/main/java/com/solvd/carservice/dao/mysql/UserDAO.java
@@ -2,13 +2,41 @@ package com.solvd.carservice.dao.mysql;
 
 import com.solvd.carservice.dao.IUserDAO;
 import com.solvd.carservice.model.User;
+import com.solvd.carservice.util.ConnectionPool;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDAO extends AbstractMysqlDAO implements IUserDAO {
+    private final static String GET_USER = "SELECT * FROM Users Where id = ?";
     @Override
     public Object getEntityById(long id) {
-        return null;
+        Connection connection = ConnectionPool.getInstance().retrieve();
+        User u = new User();
+        try (PreparedStatement statement = connection.prepareStatement(GET_USER)) {
+            statement.setLong(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    int userId = resultSet.getInt("id");
+                    String name = resultSet.getString("name");
+                    String email = resultSet.getString("email");
+                    return u;
+                } else {
+                    return null;
+                }
+            }
+        } catch (SQLException e) {
+            ConnectionPool.getInstance().putback(connection);
+            throw new RuntimeException(e);
+        } finally{
+            // TODO
+            // where to close rs and ps?
+            // do we need connection put back here too?
+        }
+
     }
 
     @Override
